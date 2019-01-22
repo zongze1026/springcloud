9731c033f4ef1fab2e8033f92c0721c52801a22f
xiezongze <xiezz@yitkeji.com>
2019-01-22 14:46:47 +0800

    add zuul

--- zuul/src/main/java/com/zongze/ZuulApplication.java
+++ zuul/src/main/java/com/zongze/ZuulApplication.java
@@ -1,17 +1,17 @@
 package com.zongze;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class ConsumerApplication {
+@EnableZuulProxy
+public class ZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 
 }
+
