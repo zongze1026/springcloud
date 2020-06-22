7c929182ebc3666b559ade460f472e5891134245
xiezongze <xiezz@yitkeji.com>
2020-06-22 09:48:02 +0800

    a

--- eureka/src/main/java/com/zongze/EurekaApplication.java
+++ eureka/src/main/java/com/zongze/EurekaApplication.java
@@ -1,5 +1,4 @@
 package com.zongze;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
