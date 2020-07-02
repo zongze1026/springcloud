02d75daf5816cb814b9e915306409b14ac0e0b34
xiezongze <13916257992@163.com>
2020-07-02 23:17:59 +0800

    'a'

---
+++ eureka/src/main/java/com/zongze/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.zongze.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Date 2020/7/2 22:16
+ * @Created by xzz
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
